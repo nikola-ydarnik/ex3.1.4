1f546e4778d107b15b2e978031579246047de471
nikola-ydarnik <nikola-ydarnik@bk.ru>
2024-04-01 13:33:50 +0300

    2

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -41,12 +41,6 @@ public class AdminController {
         return "all_users";
     }
 
-//    @GetMapping("/new")
-//    public String createNewUser(Model model) {
-//        model.addAttribute("user", new User());
-//        model.addAttribute("roles", roleSerivce.findAll());
-//        return "create_user";
-//    }
 
     @PostMapping("/create")
     public String saveNewUser(@ModelAttribute("user") @Valid User user,
@@ -54,45 +48,14 @@ public class AdminController {
                               @RequestParam(value = "rolesForController", required = false) List<String> roleName,
                               Model model) {
 
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("roles", roleSerivce.findAll());
-//            return "create_user";
-//        }
-
-        boolean isUserSaved;
-        if (roleName == null) {
-            isUserSaved = userService.saveUser(user);
-        } else {
-            isUserSaved = userService.saveUser(user,roleName);
-        }
-
-        if (isUserSaved) {
             return "redirect:/admin";
-        } else {
-            model.addAttribute("roles", roleSerivce.findAll());
-            model.addAttribute("errorMessage", "User is already exists.");
-            return "create_user";
-        }
     }
 
-//    @GetMapping("/{id}/edit")
-//    public String updateUser(Model model,
-//                             @PathVariable("id") int id) {
-//        model.addAttribute("user", userService.findUserById(id));
-//        model.addAttribute("roles", roleSerivce.findAll());
-//        return "update_user";
-//    }
 
     @PatchMapping(value ="/update/{id}")
     public String saveUpdateUser(@ModelAttribute("user") User user,
-//                                 BindingResult bindingResult,
-                                 @RequestParam(value = "rolesForController", required = false) List<String> rolesFromView,
-                                 Model model) {
+                                 @RequestParam(value = "rolesForController", required = false) List<String> rolesFromView) {
 
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("roles", roleSerivce.findAll());
-//            return "update_user";
-//        }
         System.out.println("........................................................");
         System.out.println("........................................................");
         System.out.println("........................................................");
